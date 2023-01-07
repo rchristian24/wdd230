bbff5db3eef2b18ee88702f02e241877572249e6
rchristian24 <chr22055@byui.edu>
2023-01-07 08:57:45 -0700

    wdd230 Homepage

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+//copyright year//
+const todaysdate = new Date();
+const year = todaysdate.getFullYear();
+document.querySelector("span").textContent = year
+
+//date and time document was last modified//
+document.getElementById('modified').textContent = new Date(document.lastModified);
+
+
+
